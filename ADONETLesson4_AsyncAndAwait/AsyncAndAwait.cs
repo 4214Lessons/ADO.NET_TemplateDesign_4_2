df7732369140c9ac2a7b04660d3b17c7b0ae1f3d
Tural Novruzov <turalinovruzov@gmail.com>
2023-01-19 17:16:59 +0400

    Add project files.

---
+++ ADONETLesson4_AsyncAndAwait/AsyncAndAwait.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+
+
+namespace ADONETLesson4_AsyncAndAwait;
+
+
+public partial class AsyncAndAwait : Form
+{
+    IConfigurationRoot? configurationRoot = null;
+    string conStr = string.Empty;
+
+
+    public AsyncAndAwait()
+    {
+        InitializeComponent();
+        Configure();
+    }
+
+
+    private void Configure()
+    {
+        string projectPath = Directory.GetCurrentDirectory().Split(@"bin\")[0];
+
+        configurationRoot = new ConfigurationBuilder()
+            .SetBasePath(projectPath)
+            .AddJsonFile("appsettings.json").Build();
+
+
+
+        conStr = configurationRoot.GetConnectionString("ConStr");
+    }
+}
